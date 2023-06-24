3dbb1566cacca231688dad6086bfb3ffa5e5f829
MarynaMolchanova <108984910+MarynaMolchanova@users.noreply.github.com>
2023-06-24 14:24:30 +0300

    hw

---
+++ src/components/FriendListItem/FriendListItemStyled.js
@@ -0,0 +1,8 @@
+// import styled from 'styled-components';
+
+// export const StyledStatus = styled.span`
+//   background: ${props => (props.isOnline ? 'green' : 'red')};
+//   color: black;
+//   width: 15px;
+//   height: 15px;
+//   border-radius: 50%;
